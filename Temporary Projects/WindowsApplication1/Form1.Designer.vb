d7b349599fc4e988d188ec772f3b0842fb007c3f
pmetwp@l <pmetwp@l>
2017-01-29 06:48:48 +0900

    abc

--- Temporary Projects/WindowsApplication1/Form1.Designer.vb
+++ Temporary Projects/WindowsApplication1/Form1.Designer.vb
@@ -23,6 +23,7 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.CheckedListBox1 = New System.Windows.Forms.CheckedListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'CheckedListBox1
@@ -33,11 +34,21 @@ Partial Class Form1
         Me.CheckedListBox1.Size = New System.Drawing.Size(120, 88)
         Me.CheckedListBox1.TabIndex = 0
         '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(77, 52)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.CheckedListBox1)
         Me.Name = "Form1"
         Me.Text = "Form1"
@@ -45,5 +56,6 @@ Partial Class Form1
 
     End Sub
     Friend WithEvents CheckedListBox1 As System.Windows.Forms.CheckedListBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
 
 End Class
